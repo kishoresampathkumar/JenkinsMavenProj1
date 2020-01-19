285c2bb8a9b8146d8baf0dcba811aa33f03decaf
Kishore Sampathumar <kishore.sampathkumar@gmail.com>
2020-01-20 02:57:28 +0530

    made some semantic change to 2nd iterator file.

--- PointCutRewriter.semantic_change.java
+++ PointCutRewriter.semantic_change.java
@@ -1,6 +1,6 @@
 // Write out with cheapest on left
-Iterator iter = nodes.iterator();
-Pointcut result = (Pointcut) iter.next();
+Iterator<Pointcut> iter = nodes.iterator();
+Pointcut result = iter.next();
 while (iter.hasNext()) {
-
+	Pointcut right = iter.next();
 	result = new AndPointcut(result, right);
